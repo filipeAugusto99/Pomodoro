163733a031e2a56a8ab297a58075afe2a0257275
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-08 12:16:13 -0300

    Ui setup

---
+++ main.py
@@ -0,0 +1,53 @@
+from tkinter import *
+from wsgiref.validate import bad_header_value_re
+
+from numpy.random import normal
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Create Timer Label
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+timer_label.grid(column=1, row=0)
+
+# Create Canvas
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+canvas.grid(column=1, row=1)
+
+# Create Start Button
+start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"))
+start_button.grid(column=0, row=2)
+
+#Create a Check Label
+
+check_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=("normal", 15))
+check_label.grid(column=1, row=3)
+
+# Create Reset Button
+reset_button = Button(text="Reset", font=(FONT_NAME, 10, "bold"))
+reset_button.grid(column=2, row=2)
+
+
+
+window.mainloop()
