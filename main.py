e5d3bceede2b65b4965d7f457f50261e7c244c2c
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-08 13:48:02 -0300

    Create a count_down func

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from tkinter import *
-from wsgiref.validate import bad_header_value_re
-
-from numpy.random import normal
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -18,6 +15,11 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -32,8 +34,10 @@ timer_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.grid(column=1, row=1)
+
+count_down(5)
 
 # Create Start Button
 start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"))
