b90776adacab426583fc111502f4d31aae9e9b6b
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-08 17:02:15 -0300

    Timer Mechanics

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -13,10 +14,24 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count / 60)
+    count_seg = count % 60
+
+
+    if count_seg < 10:
+        count_seg = f"0{count_seg}"
+
+
+
+
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_seg}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -37,10 +52,8 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.grid(column=1, row=1)
 
-count_down(5)
-
 # Create Start Button
-start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"))
+start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"), command=start_timer)
 start_button.grid(column=0, row=2)
 
 #Create a Check Label
